b2f3be85155232a090368d473720d346689d2702
Nicholas <madkog77@gmail.com>
2021-03-22 12:20:31 +1100

    remove redundant files

--- init.sql
+++
@@ -1,2 +0,0 @@
-CREATE DATABASE IF NOT EXISTS referral_app;
-USE referral_app;
--- init_db.sh
+++
@@ -1,4 +0,0 @@
-RUN /bin/bash -c "/usr/bin/mysqld_safe --skip-grant-tables &" && \
-  sleep 5 && \
-  mysql -u root -e "CREATE DATABASE referral_app" && \
-  mysql -u root mydb < /tmp/dump.sql
